4cb78ba86540193ee347063619f05ccc55468c76
Douglass Turner <douglass.turner@gmail.com>
2018-09-27 09:02:03 -0400

    refactor

--- js/renderer.js
+++ js/renderer.js
@@ -1,31 +1,31 @@
 class Renderer {
     constructor($container) {
 
-        let svg = SVG($container.get(0));
+        this.svg = SVG($container.get(0));
 
         let svg_dimension = $container.width();
-        svg.size(svg_dimension, svg_dimension);
+        this.svg.size(svg_dimension, svg_dimension);
 
-        let root_group = svg.group();
+        let root_group = this.svg.group();
 
         let margin = 0.05;
         root_group.transform({ x:margin * svg_dimension, y:margin * svg_dimension });
 
         this.canvas_dimension = 0.9 * $container.width();
 
-        let canvas = svg.rect(this.canvas_dimension, this.canvas_dimension);
+        let canvas = this.svg.rect(this.canvas_dimension, this.canvas_dimension);
         root_group.add(canvas);
 
         canvas.attr( { fill:'white' } );
 
-        let drawing_origin_group = svg.group();
+        let drawing_origin_group = this.svg.group();
         root_group.add(drawing_origin_group);
 
         // drawing_origin_group.transform({ x:this.canvas_dimension/2, y:(this.canvas_dimension-radius) });
         drawing_origin_group.transform({ x:this.canvas_dimension/2, y:this.canvas_dimension/2 });
 
         // svg.circle(diameter)
-        let origin = svg.circle();
+        let origin = this.svg.circle();
         drawing_origin_group.add(origin);
 
         let radius = 8;
@@ -33,12 +33,19 @@ class Renderer {
 
         origin.attr( { fill:'gainsboro', 'stroke':'dimgray', 'stroke-width':1 });
 
-        let line_width = 4;
-        let line = svg.line(0, 0, this.canvas_dimension/4, -this.canvas_dimension/3);
-        line.stroke({ 'color':'red', 'width':line_width, 'linecap':'round' });
-        drawing_origin_group.add(line);
+        drawing_origin_group.add(this.drawLine(0, 0, this.canvas_dimension/4, -this.canvas_dimension/3));
 
     }
+
+    drawLine (xStart, yStart, xEnd, yEnd) {
+
+        const line_width = 4;
+        const line = this.svg.line(xStart, yStart, xEnd, yEnd);
+
+        line.stroke({ color:'red', width:line_width, linecap:'round' });
+
+        return line;
+     }
 
     drawingDimension () {
         return this.canvas_dimension / 32.0;
