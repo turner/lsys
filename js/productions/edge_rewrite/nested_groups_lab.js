d39b9b92c9d621cc58f41df9f939bc94cff5ed4c
Douglass Turner <douglass.turner@gmail.com>
2018-10-07 07:43:53 -0400

    now rendering proper hierarchical scene graph with transforms at branch bases. YAASSS

--- js/productions/edge_rewrite/nested_groups_lab.js
+++ js/productions/edge_rewrite/nested_groups_lab.js
@@ -1,24 +1,24 @@
 
 export const production =
     {
-        angle: 30,
+        angle: 22.5,
 
         axiom:
             {
                 string: 'X',
                 generation: 0
             },
-        productions:
-            {
-                X: 'F[+X]+F-F',
-                F: 'FF'
-            }
-
         // productions:
         //     {
-        //         X: 'F-[[X]+X]+F[+FX]-X',
+        //         X: 'F[+X]+F-F',
         //         F: 'FF'
         //     }
+
+        productions:
+            {
+                X: 'F-[[X]+X]+F[+FX]-X',
+                F: 'FF'
+            }
 
         // productions:
         //     {
